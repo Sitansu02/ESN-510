03f3f00b862db3f690b9702a0c041c39cb605192
Sitansu02 <140053440+Sitansu02@users.noreply.github.com>
2023-08-28 12:38:41 +0530

    Add files via upload

---
+++ sitansu 23612013.m
@@ -0,0 +1,23 @@
+for n=1:10
+    disp(n)
+end
+%%
+for x=2:2:100
+    disp(x)
+end
+%%
+z=0
+for n=1:3
+    x =2*n^2+n+1;
+    disp(x)
+    z=z+x
+   disp(z)
+
+
+end
+%%
+for n=1:5
+    if n>3
+        disp(n)
+    end
+end
